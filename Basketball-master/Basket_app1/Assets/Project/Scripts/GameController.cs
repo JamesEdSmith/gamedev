bdc4db64716bd7eff274a9714a2a7d866e9a35d6
James Edward Smith <roomforadventure@gmail.com>
2020-05-05 09:03:11 -0400

    WIP

--- Basketball-master/Basket_app1/Assets/Project/Scripts/GameController.cs
+++ Basketball-master/Basket_app1/Assets/Project/Scripts/GameController.cs
@@ -6,23 +6,27 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour {
 
 	public Player player;
-	public float resetTimer = 5f;
+    public GameObject floor;
+    public GameObject marker;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (player.holdingBall == false ){
-			resetTimer -= Time.deltaTime;
-			if (resetTimer <= 0){
-				SceneManager.LoadScene ("Game");
-			}
+        // create a ray from the mousePosition
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // plane.Raycast returns the distance from the ray start to the hit point
+        float distance = 1000;
+        if (floor.GetComponent<BoxCollider>().Raycast(ray, out RaycastHit info, distance))
+        {
+            var hitPoint = info.point;
+            marker.transform.position = hitPoint;
 
-		}
-		
-	}
+        }
+
+    }
 }
--- Basketball-master/Basket_app1/Assets/Project/Scripts/Player.cs
+++ Basketball-master/Basket_app1/Assets/Project/Scripts/Player.cs
@@ -2,40 +2,132 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    const float MAX_MOVEMEN_SPEED = 12;
+    public Ball ball;
 
-	public Ball ball;
-	public GameObject playerCamara;
-	public float ballDistance = 2f;
-	public float ballThrowingForce = 5f;
+    public float ballDistance = 1f;
+    public float ballThrowingForce;
+    public GameObject floor;
 
+    public bool holdingBall = true;
 
-	public bool holdingBall = true;
+    //values that will be set in the Inspector
+    public Transform target;
+    public float RotationSpeed;
 
+    //values for internal use
+    private Quaternion _lookRotation;
+    private Vector3 _direction;
+    public float sqrMagnitude;
+    public float movementSpeed = 0;
+    public float decellMagnitude = 20;
 
-	// Use this for initialization
-	void Start () {
-		ball.GetComponent<Rigidbody>().useGravity = false;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(holdingBall){
-			ball.transform.position = playerCamara.transform.position + playerCamara.transform.forward * ballDistance;
+    public float angleToBasket;
+    private float gravity = Physics.gravity.y;
 
-			//clic mouse
-
-			if(Input.GetMouseButtonDown(0)){
-				holdingBall = false;
-				ball.ActivateTrail ();
-				ball.GetComponent<Rigidbody>().useGravity = true;
-				ball.GetComponent<Rigidbody>().AddForce(playerCamara.transform.forward * ballThrowingForce);
-
-			}
+    public Transform basket;
 
 
-		}
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingBall)
+        {
+            ball.transform.position = transform.position + transform.forward * ballDistance;
+            Vector3 target = basket.TransformPoint(Vector3.zero);
+            float y = target.y - transform.position.y;
+            target.y = 0;
+            float x = (target - transform.position).sqrMagnitude;
+            
+            //calc ball angle
+            float wofkep = gravity * (gravity * Mathf.Pow(x, 2) + 2f * y * Mathf.Pow(ballThrowingForce, 2));
+            float i = (Mathf.Pow(ballThrowingForce, 2) + Mathf.Sqrt(Mathf.Pow(ballThrowingForce, 4) - gravity * (gravity * Mathf.Pow(x, 2) + 2f * y * Mathf.Pow(ballThrowingForce, 2))));
+            float angle = (Mathf.Pow(ballThrowingForce, 2) + Mathf.Sqrt(Mathf.Pow(ballThrowingForce, 4) - gravity * (gravity * Mathf.Pow(x, 2) + 2f * y * Mathf.Pow(ballThrowingForce, 2)))) / (gravity * x);
+            angleToBasket = Mathf.Atan(angle);
+            if (float.IsNaN(angleToBasket))
+            {
+                angle = 0;
+            }
+            else
+            {
+                Debug.Log("tell me");
+            }
+
+            
+            //clic mouse
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                holdingBall = false;
+                ball.ActivateTrail();
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                Transform transformRotation = transform;
+                transformRotation.Rotate(Mathf.Rad2Deg * angleToBasket, 0, 0);
+                ball.GetComponent<Rigidbody>().velocity = transformRotation.forward * ballThrowingForce;
+                //Quaternion.AngleAxis(Mathf.Rad2Deg * angleToBasket, Vector3.left) 
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ball.GetComponent<Rigidbody>().useGravity = false;
+            ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            holdingBall = true;
+        }
+
+        //find the vector pointing from our position to the target
+        Vector3 targetPosition = target.position;
+        targetPosition.y = transform.position.y;
+        _direction = (targetPosition - transform.position).normalized;
+
+        //create the rotation we need to be in to look at the target
+        _lookRotation = Quaternion.LookRotation(_direction);
+
+        //rotate us over time according to speed until we are in the required rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+
+        sqrMagnitude = (targetPosition - transform.position).sqrMagnitude;
+        if (sqrMagnitude > 1)
+        {
+
+            if (movementSpeed < MAX_MOVEMEN_SPEED)
+            {
+                movementSpeed += 8f * Time.deltaTime;
+            }
+            else
+            {
+                movementSpeed = MAX_MOVEMEN_SPEED;
+            }
+            if (sqrMagnitude < decellMagnitude)
+            {
+                movementSpeed = sqrMagnitude / decellMagnitude * MAX_MOVEMEN_SPEED;
+            }
+
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        }
+        else
+        {
+            if (movementSpeed > 0)
+            {
+                movementSpeed -= 10f * Time.deltaTime;
+            }
+            else
+            {
+                movementSpeed = 0;
+            }
+        }
+
+    }
+
+
 }
